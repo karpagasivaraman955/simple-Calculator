ece8b8269812f76567d7238f47a7dfc023a35aba
Karpaga sivaraman D S <144925594+karpagasivaraman955@users.noreply.github.com>
2024-06-21 14:54:56 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+n1 = int(input("Enter a number 1 : "))
+n2 = int(input("Enter a number 2 : "))
+add = n1 + n2
+sub = n1 - n2
+multi = n1 * n2
+div = n1 / n2
+mod = n1 // n2
+
+print("Addition =", add)
+print(sub, "Subbraction = ")
+print('Multiplication = ', multi)
+print("Division = ", div)
+print('Modulus = ', mod)
